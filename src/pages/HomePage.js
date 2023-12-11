699b1d346a40b2db4ac5eef04aa2f789a6da1c60
Fred Nobre <frednobre18@gmail.com>
2023-12-11 18:40:33 +0100

    :fire: Modify Homepage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import bglogin from "../../public/bglogin.jpg";
 import Image from "next/image";
+import colors from "tailwindcss/colors";
 import { Navbar } from "@/components/Navbar";
 
 const HomePage = () => {
   return (
     <>
-      <Navbar />
       <main className="h-screen flex justify-center">
         <Image
           className="h-full absolute z-[-1] blur-sm brightness-50 "
