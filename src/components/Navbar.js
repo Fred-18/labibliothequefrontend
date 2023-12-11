d98ad4e4eb38209a9319b73e493e09b1e860aa78
Fred Nobre <frednobre18@gmail.com>
2023-12-11 18:38:35 +0100

    :sparkles:  Navbar components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,72 +1,20 @@
-import { Container } from "postcss";
 import React from "react";
-import { useState } from "react";
-import Image from "next/image";
-import livre from "../../public/livre.png";
-import burger from "../../public/burger.png";
 import Link from "next/link";
 
 export const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
 
-  function toogleMenu() {
-    setToggleMenu;
-    console.log(toggleMenu);
-  }
-  return (
-    <>
-      <div href="top" className="flex gap-10" aria-label="Page d'acceuil">
-        <nav
-          className="bg-white maw-w-5Xl mx-auto font-poppins p-6 flex items-center justify-between gap
-        text-black"
-        >
-          <Image
-            className="w-10 p-0 hidden md:w-7 md:inline md:mr-4"
-            src={livre}
-            alt="logo livre"
-          />
-          <span className="inline text-lg lg:text -xl">La Biblio</span>
-        </nav>
-        <button
-          onClick={toogleMenu}
-          aria-label="toggle button"
-          aria-expanded="false"
-          className="cursor-pointer w-7 md:hidden"
-        >
-          <Image src={burger} alt="menu"></Image>
-        </button>
-        <ul
-          className="w-full absolute top-full left-0 -translate-full -z-10
-        text-black border-b border-gray-200 flex flex-col items-center md:static
-        md:z-10 :w-min md:transform-none md:border-none md:flex-row"
-        >
-          <li className="py-4 md:py-0 md:mr-6">
-            <Link
-              href={"/SubscribePage"}
-              className="text-sm uppercase font-semibold w-full hover:text-rose-600"
-            >
-              Inscription
-            </Link>
-          </li>
+    return (
+        <>
+            <nav className="bg-white text-black">
+                <h1 className="text-center lg:text-start">The library</h1>
+                <ul className="flex justify-between">
+                    <li className="collapse md:visible lg:visible hover:text-red-500"><Link href='Accueil'>Home</Link></li>
+                    <li className="collapse md:visible lg:visible hover:text-red-500"><Link href='LoginPage'>Login</Link></li>
+                    <li className="collapse md:visible lg:visible hover:text-red-500"><Link href='SubscribePage'>Inscription</Link></li>
+                    <button className=" md:collapse lg:collapse flex justify-start ">click</button>
+                </ul>
 
-          <li className="py-4 md:py-0 md:mr-6">
-            <Link
-              href={""}
-              className="text-sm uppercase font-semibold w-full hover:text-rose-600"
-            >
-              fred
-            </Link>
-          </li>
-          <li className="py-4 md:py-0 md:mr-6">
-            <Link
-              href={"#"}
-              className="text-sm uppercase font-semibold w-full hover:text-rose-600"
-            >
-              fred
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+            </nav>
+        </>
+    );
 };
