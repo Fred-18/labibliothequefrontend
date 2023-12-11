4dd6a181159ca0958fdf7b8272bc75a2ddc0f5af
Fred Nobre <frednobre18@gmail.com>
2023-12-11 18:40:07 +0100

    :fire: Modify documents.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,13 @@
-import { Header } from "@/components/Header";
+//import { Header } from "@/components/Header";
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { Navbar } from "@/components/Navbar";
+import Header from "@/components/Header";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Header />
       <body>
-        <Header />
         <Main />
         <NextScript />
       </body>
